e051cc230d86a4194d2b5fb26ccc721916cb7c8b
Slightly Nervous Doggo <github@slightlynervousdoggo.com>
2020-07-09 12:10:36 -0400

    Refactored

--- commands/profile.js
+++ commands/profile.js
@@ -20,95 +20,80 @@ module.exports = {
     }
     const createdAt = format(user.createdAt, 'MMM do yyyy, H:mm:ss')
     const createdAtFromNow = formatDistance(new Date(), user.createdAt)
-
+    let fields;
     if (guildMember) {
       const joinedAt = format(guildMember.joinedAt, 'MMM do yyyy, H:mm:ss')
       const joinedAtFromNow = formatDistance(new Date(), guildMember.joinedAt)
-      try {
-        message.channel.send({
-          embed: {
-            color: 3447003,
-            title: 'User Profile',
-            description: `User data for <@${user.id}>`,
-            thumbnail: {
-              url: user.displayAvatarURL()
-            },
-            fields: [
-              {
-                name: 'Username',
-                value: user.tag,
-                inline: true
-              },
-              {
-                name: 'ID',
-                value: user.id,
-                inline: true
-              },
-              {
-                name: 'Status',
-                value: guildMember.presence.status,
-                inline: true
-              },
-              {
-                name: 'Highest Role',
-                value: guildMember.roles.highest,
-                inline: true
-              },
-              {
-                name: 'Created',
-                value: `${createdAt} 
-                      (${createdAtFromNow})`,
-                inline: true
-              },
-              {
-                name: 'Joined',
-                value: `${joinedAt}
-                      (${joinedAtFromNow})`,
-                inline: true
-              }
-            ],
-            timestamp: new Date()
-          }
-        });
-      } catch (e) {
-        message.reply(`Sorry ${message.author}, I couldnt display this profile because of : ${e}`)
-        console.log(e)
-      }
-    } else {
-      try {
-        message.channel.send({
-          embed: {
-            color: 3447003,
-            title: 'User Profile',
-            description: `User data for <@${user.id}>`,
-            thumbnail: {
-              url: user.displayAvatarURL()
-            },
-            fields: [
-              {
-                name: 'Username',
-                value: user.tag,
-                inline: true
-              },
-              {
-                name: 'ID',
-                value: user.id,
-                inline: true
-              },
-              {
-                name: 'Created',
-                value: `${createdAt} 
+      fields = [
+        {
+          name: 'Username',
+          value: user.tag,
+          inline: true
+        },
+        {
+          name: 'ID',
+          value: user.id,
+          inline: true
+        },
+        {
+          name: 'Status',
+          value: guildMember.presence.status,
+          inline: true
+        },
+        {
+          name: 'Highest Role',
+          value: guildMember.roles.highest,
+          inline: true
+        },
+        {
+          name: 'Created',
+          value: `${createdAt} 
                 (${createdAtFromNow})`,
-                inline: true
-              }
-            ],
-            timestamp: new Date()
-          }
-        });
-      } catch (e) {
-        message.reply(`Sorry ${message.author}, I couldnt display this profile because of : ${e}`)
-        console.log(e)
-      }
+          inline: true
+        },
+        {
+          name: 'Joined',
+          value: `${joinedAt}
+                (${joinedAtFromNow})`,
+          inline: true
+        }
+      ]
+    } else {
+      fields = [
+        {
+          name: 'Username',
+          value: user.tag,
+          inline: true
+        },
+        {
+          name: 'ID',
+          value: user.id,
+          inline: true
+        },
+        {
+          name: 'Created',
+          value: `${createdAt} 
+          (${createdAtFromNow})`,
+          inline: true
+        }
+      ]
+    }
+    try {
+      message.channel.send({
+        embed: {
+          color: 3447003,
+          title: 'User Profile',
+          description: `User data for <@${user.id}>`,
+          thumbnail: {
+            url: user.displayAvatarURL()
+          },
+          fields: fields,
+          timestamp: new Date()
+        }
+      });
+    } catch (e) {
+      message.reply(`Sorry ${message.author}, I couldnt display this profile because of : ${e}`)
+      console.log(e)
     }
   }
 }
