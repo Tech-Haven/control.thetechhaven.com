50f7c0ae49e0f50c1b20614cdee5bbeba2997a57
Slightly Nervous Doggo <github@slightlynervousdoggo.com>
2020-10-12 14:19:03 -0400

    Added ability to register for lab

--- commands/labLogin.js
+++ commands/labLogin.js
@@ -1,63 +1,74 @@
-const { labLogin } = require('../utils/lab')
-const User = require('../models/User')
-const LabUser = require('../models/LabUser')
+const { labLogin } = require('../utils/lab');
+const User = require('../models/User');
+const LabUser = require('../models/LabUser');
 
 module.exports = {
   name: 'lab-login',
   description: 'Login to the lab server for use with Discord',
   args: true,
-  usage: `<username> <password>`,
+  usage: `<password>`,
   async execute(message, args) {
-
-    if (!args[0] || !args[1]) {
-      return message.reply("Username and password are required!")
+    if (!args[0]) {
+      return message.reply('Password is required!');
     }
 
-    const username = args[0];
-    const password = args[1];
+    const username = message.author.id;
+    const password = args[0];
 
     try {
-      let discordUser = await User.findOne({ _id: message.author.id })
+      let discordUser = await User.findOne({ _id: message.author.id });
 
       // save discord user to the database if they don't exist
       if (!discordUser) {
         newUser = new User({
           _id: message.author.id,
           username: message.author.username,
-          discriminator: message.author.discriminator
-        })
+          discriminator: message.author.discriminator,
+        });
         newUser = await newUser.save();
-        discordUser = newUser
+        discordUser = newUser;
       }
 
       // Get back labUser from db
       const labUser = await labLogin(username, password);
 
+      console.log(labUser);
+
       // Invalid creds
       if (labUser.error) {
-        return message.reply(`Error! ${labUser.error}`)
+        return message.reply(`Error! ${labUser.error}`);
       }
 
       // user already logged in before, or another user is trying to log in as them
       if (labUser.discord_user) {
         if (labUser.discord_user == message.author.id) {
-          return message.reply(`You already logged in!`)
-        } {
-          message.reply(`Lab account is already associated with another Discord account. You got caught ;)`)
+          return message.reply(`You already logged in!`);
+        }
+        {
+          message.reply(
+            `Lab account is already associated with another Discord account. You got caught ;)`
+          );
           // DM the actual owner that someone tried to login to their lab account
-          const owner = await message.client.users.fetch(labUser.discord_user)
-          return await owner.send(`${message.author} tried signing into your lab account. Reset your password!`)
+          const owner = await message.client.users.fetch(labUser.discord_user);
+          return await owner.send(
+            `${message.author} tried signing into your lab account. Reset your password!`
+          );
         }
       }
 
       // User hasn't associated theirDiscord account yet, so update their labUser to reference discordUser
-      const updatedLabUser = await LabUser.findOneAndUpdate({ userID: labUser.userID }, { discord_user: discordUser }, { upsert: true, new: true });
+      const updatedLabUser = await LabUser.findOneAndUpdate(
+        { userID: labUser.userID },
+        { discord_user: discordUser },
+        { upsert: true, new: true }
+      );
 
-      message.reply(`Success! Discord account is now associated with your lab account.`)
-
+      message.reply(
+        `Success! Discord account is now associated with your lab account.`
+      );
     } catch (error) {
-      console.error(error)
-      message.reply(`Error! ${error}`)
+      console.error(error);
+      message.reply(`Error! ${error}`);
     }
-  }
-}
+  },
+};
---
+++ commands/labRegister.js
@@ -0,0 +1,31 @@
+const { labRegister } = require('../utils/lab');
+
+module.exports = {
+  name: 'lab-register',
+  description: 'Register to the lab server for use with Discord',
+  args: true,
+  usage: `<password>`,
+  async execute(message, args) {
+    if (!args[0]) {
+      return message.reply('Password is required!');
+    }
+
+    const username = message.author.id;
+    const password = args[0];
+
+    try {
+      const status = await labRegister(username, password);
+
+      if (status.error) {
+        return message.reply(`Error! ${status.error.msg}`);
+      }
+
+      message.reply(
+        'Lab account created. Please login with the `lab-login` command.'
+      );
+    } catch (error) {
+      console.error(error);
+      message.reply(`Error! ${error}`);
+    }
+  },
+};
