49bf852ab7ce45514e8c1c075b5f1bb8eecac88e
Slightly Nervous Doggo <github@slightlynervousdoggo.com>
2021-04-08 16:40:19 -0400

    Forgot a vault request..

--- controllers/vault/helpers/createSecret.js
+++ controllers/vault/helpers/createSecret.js
@@ -1,15 +1,17 @@
 const axios = require('axios');
-
+const https = require('https');
 const VAULT_URL = process.env.VAULT_URL;
 
 const createSecret = async (id, secret, user, token) => {
   try {
+    const agent = new https.Agent({ rejectUnauthorized: false });
     const config = {
       method: 'post',
       url: `${VAULT_URL}/v1/lab.thetechhaven.com/data/${user}`,
       headers: {
         'X-Vault-Token': token,
       },
+      httpsAgent: agent,
       data: {
         data: {
           application_credential_id: id,
